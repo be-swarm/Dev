e200144e389bdfc54edab4a3e4c8872f12e69c57
be-swarm <t.roustan@beswarm.fr>
2022-10-11 17:45:24 +0200

    initial

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site21',
+  title: 'My Site212',
   tagline: 'Dinosaurs are cool',
     url: 'https://github.com/',
     baseUrl: '/Dev/',
