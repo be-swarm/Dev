58141f6de5ea49088be39bc67890b4fcbaa5dc98
be-swarm <t.roustan@beswarm.fr>
2022-10-11 17:58:20 +0200

    initial

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,14 +6,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site212',
-  tagline: 'Dinosaurs are cool',
+  title: 'Be Swarm',
+  tagline: 'Under construction',
     url: 'https://github.com/',
     baseUrl: '/Dev/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
-// GitHub pages deployment config.
+    // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'be-swarm', // Usually your GitHub org/user name.
     projectName: 'Dev', // Usually your repo name.
@@ -37,15 +37,15 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -116,12 +116,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/be-swarm',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Be Swarm.`,
       },
       prism: {
         theme: lightCodeTheme,
